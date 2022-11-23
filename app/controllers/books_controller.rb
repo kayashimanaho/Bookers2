935748812d534596430ef468d4b65c20f993f634
kayashimanaho <116855181+kayashimanaho@users.noreply.github.com>
2022-11-23 08:07:02 +0000

    ファイル作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,19 @@
 class BooksController < ApplicationController
-  def top
+  
+  def new
+    @books = Book.new
+  end
+  
+  def create
+    @books = Book.new(book_params)
+    @books.user_id = current_user.id
+    @books.save
+    redirect_to books_path
+  end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:book, :image, :caption)
   end
 end
