036a47b1ce701507ce72283b69cf9c31f1a71353
kayashimanaho <116855181+kayashimanaho@users.noreply.github.com>
2022-11-27 15:50:02 +0000

    show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
     
     def index
+     　@users = User.all
+     　@user = current_
     end
+
+   def show
+     @user = User.find(params[:id])
+     @book = Book.new
+     @books = @user.books
+   end
+
+   def edit
+   end
 end
+
